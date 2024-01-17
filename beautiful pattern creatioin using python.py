618c6baa510327db86102adf24c22577114dacc6
Najaf Ali Alkhani <150806918+Najaf-Ali12@users.noreply.github.com>
2024-01-17 07:44:34 +0500

    Add files via upload

---
+++ beautiful pattern creatioin using python.py
@@ -0,0 +1,39 @@
+for i in range(6,0,-1):
+  
+  for j in range(0,i):
+    print("$",end=" ")
+  print(" ")
+#this will make a figure like this
+#    $ $ $ $ $ $  
+#    $ $ $ $ $  
+#    $ $ $ $  
+#    $ $ $  
+#    $ $  
+#    $ 
+
+for i in range(0,6):
+  for j in range(i+1,0,-1):
+    print("$",end=" ")
+  print(" ")
+#this will make a figure like this
+#    $  
+#    $ $ $  
+#    $ $ $ $  
+#    $ $ $ $ $  
+#    $ $ $ $ $ $
+
+
+
+#there combination will make something like this
+#$ $ $ $ $ $  
+#$ $ $ $ $  
+#$ $ $ $  
+#$ $ $  
+#$ $  
+#$  
+#$  
+#$ $  
+#$ $ $  
+#$ $ $ $  
+#$ $ $ $ $  
+#$ $ $ $ $ $  
