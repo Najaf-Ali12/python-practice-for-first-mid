81e18aaefcbad8c6c086ac77a622fddc30d3c4d4
Najaf Ali Alkhani <150806918+Najaf-Ali12@users.noreply.github.com>
2024-01-17 07:45:32 +0500

    Add files via upload

---
+++ swap values.py
@@ -0,0 +1,13 @@
+a=input("enter the value of a ")
+b=input("enter the value of b")
+#c=b
+#d=a
+#a=c
+#b=d
+#or can be done like this
+var=a
+a=b
+b=var
+#here at place of formatting string catenation can be done
+print("a="+a)
+print("b="+b)
